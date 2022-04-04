08ce4570c4530bc62cfddb351c01efa98c2b15ca
sumitguptaa6 <sumitguptaa6@gmail.com>
2022-04-04 17:30:20 +0530

    11th commit

--- src/main/java/com/ibm/lms/controller/BooksController.java
+++ src/main/java/com/ibm/lms/controller/BooksController.java
@@ -1,16 +1,21 @@
 package com.ibm.lms.controller;
 
+import java.io.File;
 import java.util.List;
 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ibm.lms.entity.Books;
 import com.ibm.lms.services.Books_Services;
@@ -31,6 +36,19 @@ public class BooksController {
 		 return "Book Already Existed!!!";
 	 }
 	 
+	}
+	
+	@PostMapping("/upload")
+	public ResponseEntity<?> fileUpload(@RequestParam("file1") MultipartFile file1,@RequestParam("file2") MultipartFile file2){
+		String fileName1 = file1.getOriginalFilename();
+		String fileName2 = file2.getOriginalFilename();
+		try {
+			file1.transferTo( new File("C:\\Users\\sumit\\Documents\\Ibm_final\\Frontend\\IBM-CaseStudy\\src\\assets\\books\\" + fileName1));
+			file2.transferTo( new File("C:\\Users\\sumit\\Documents\\Ibm_final\\Frontend\\IBM-CaseStudy\\src\\assets\\books\\" + fileName2));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok("File upload successfully");	
 	}
 	
 	@GetMapping(value = "/getbooks")
